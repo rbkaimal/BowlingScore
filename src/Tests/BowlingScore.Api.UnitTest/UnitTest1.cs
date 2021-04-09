6b288b6e3cc50b72c1d8ad88ca6238215f11a8a5
Ragesh Kaimal <kaimal.ragesh@gmail.com>
2021-04-10 07:13:51 +1000

    Initial commit: Created basic solution and projects

---
+++ src/Tests/BowlingScore.Api.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace BowlingScore.Api.UnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
